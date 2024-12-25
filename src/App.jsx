13e02a0f23dd42f64576a5b922e4f969c3846237
Kabileshwaran183 <Kabileshwaran183@gmail.com>
2024-12-25 10:08:40 +0530

    header details - static contents of our project

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,11 @@ function App() {
   return (
     <>
       <main>
-        main element goes here
+            <header>
+                <h1>Assembly: Endgame</h1>
+                <p>Guess the word within 8 attempts to keep the 
+                programming world safe from Assembly!</p>
+            </header>
       </main>
     </>
   )
